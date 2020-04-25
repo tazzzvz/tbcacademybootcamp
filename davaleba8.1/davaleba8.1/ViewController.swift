c3cc64956da3a420f71c67773896dc2bd99778d0
tazzzvz <57710405+tazzzvz@users.noreply.github.com>
2020-04-25 19:22:29 +0400

    Add files via upload

---
+++ davaleba8.1/davaleba8.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  davaleba8.1
+//
+//  Created by Tazo Japaridze on 4/25/20.
+//  Copyright © 2020 Tbc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
